e6ff08d05eaf75a65e14ff7fe69a4e9e50024eb6
raven <cy6430@126.com>
2016-10-06 16:06:23 +0800

    第一次提交

---
+++ app/src/main/java/smv/lovearthstudio/com/svmproj/fragment/CollectioniFragment.java
@@ -0,0 +1,30 @@
+package smv.lovearthstudio.com.svmproj.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import smv.lovearthstudio.com.svmproj.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CollectioniFragment extends Fragment {
+
+
+    public CollectioniFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_collectioni, container, false);
+    }
+
+}
